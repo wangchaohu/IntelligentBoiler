56c2807e51dd126207797abe8d68dfd3ee89e207
wangchaohu <xwangch@yeah.net>
2017-01-02 22:41:25 +0800

    IntelligentBoiler version 1

---
+++ app/src/main/java/com/lapsen/intelligentboiler/interfaces/MainActivityInterface.java
@@ -0,0 +1,15 @@
+package com.lapsen.intelligentboiler.interfaces;
+
+/**
+ * Created by wangchaohu on 2017/1/2.
+ */
+
+public interface MainActivityInterface {
+    public void showProgress();
+    public void hideProgress();
+
+    public void toOtherActivity();
+
+    public void showDialog();
+    public void hidDialog();
+}
