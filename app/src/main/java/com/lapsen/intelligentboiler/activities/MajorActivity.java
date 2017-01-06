41505ea3ae3a3bd3b710bbff6ca2ce285732adb0
wangchaohu <xwangch@yeah.net>
2017-01-06 19:18:37 +0800

    优化gridView布局，初步完成

--- app/src/main/java/com/lapsen/intelligentboiler/activities/MajorActivity.java
+++ app/src/main/java/com/lapsen/intelligentboiler/activities/MajorActivity.java
@@ -10,7 +10,6 @@ import com.lapsen.intelligentboiler.base.BaseActivity;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Created by lapsen_wang on 2017/1/5.
@@ -22,14 +21,15 @@ public class MajorActivity extends BaseActivity {
     private GridView mGridView;
     private SimpleAdapter mSimpleAdapter;
 
-    private ArrayList<Map<String, Integer>> listData;
+    private ArrayList<Map<String, Object>> listData = new ArrayList<>();
     private int[] icons = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher}; //GridView上的图片
-    private int[] texts = {R.string.dialog_text_yes, R.string.dialog_text_yes, R.string.dialog_text_yes, R.string.dialog_text_yes, R.string.dialog_text_yes, R.string.dialog_text_yes}; //GridView上的文字
+    private String[] texts = {"直接前往", "直接前往", "直接前往", "直接前往", "直接前往", "直接前往"}; //GridView上的文字
 
     @Override
     public void initViews(Bundle savedInstanceState) {
         setContentView(R.layout.activity_major);
         initToolBar("锅炉");
+        init();
     }
 
     /**
@@ -37,7 +37,9 @@ public class MajorActivity extends BaseActivity {
      */
     private void init() {
         mGridView = (GridView) findViewById(R.id.major_Gv);
-        setGridView(mGridView);
+        if (null != mGridView) {
+            setGridView(mGridView);
+        }
     }
 
     /**
@@ -51,16 +53,16 @@ public class MajorActivity extends BaseActivity {
          * */
         if (icons.length == texts.length) {
             for (int i = 0; i < icons.length; i++) {
-                HashMap<String, Integer> map = new HashMap<>();
+                HashMap<String, Object> map = new HashMap<>();
                 map.put("image", icons[i]);
                 map.put("text", texts[i]);
                 listData.add(map);
             }
         }
 
-        if (null != mGridView && listData.size() != 0) {
-            mSimpleAdapter = new SimpleAdapter(this, listData, R.layout.item_major_gridview, new String[]{"image", "text"}, new int[]{R.id.major_grid_Iv, R.id.major_grid_Tv});
+//        if (null != mGridView && listData.size() != 0) {
+            mSimpleAdapter = new SimpleAdapter(MajorActivity.this, listData, R.layout.item_major_gridview, new String[]{"image", "text"}, new int[]{R.id.major_grid_Iv, R.id.major_grid_Tv});
             mGridView.setAdapter(mSimpleAdapter);
-        }
+//        }
     }
 }
