45da968c4a867440fa83c630085258b797cfcd1b
wangchaohu <xwangch@yeah.net>
2017-01-22 14:03:02 +0800

    去除屏幕适配

--- app/src/main/java/com/lapsen/intelligentboiler/activities/MajorActivity.java
+++ app/src/main/java/com/lapsen/intelligentboiler/activities/MajorActivity.java
@@ -71,7 +71,7 @@ public class MajorActivity extends BaseActivity implements MajorActivityInterfac
                     inflated.findViewById(R.id.btn_choice).setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            toOtherActivity(SecurityAlarmActivity.class);
+                            toOtherActivity(SelectDemonstrateActivity.class);
                         }
                     });
                 }
