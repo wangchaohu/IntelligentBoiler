d27a7b6f8a5854a571124612d3c8fca38a7115e0
wangchaohu <xwangch@yeah.net>
2017-03-15 10:50:43 +0800

    添加动态权限获取

--- app/src/main/java/com/lapsen/intelligentboiler/app/IBApplication.java
+++ app/src/main/java/com/lapsen/intelligentboiler/app/IBApplication.java
@@ -1,6 +1,8 @@
 package com.lapsen.intelligentboiler.app;
 
+
 import android.app.Application;
+
 
 /**
  * Created by lapsen_wang on 2017/3/9/0009.
@@ -11,11 +13,5 @@ public class IBApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        if (LeakCanary.isInAnalyzerProcess(this)) {
-//            // This process is dedicated to LeakCanary for heap analysis.
-//            // You should not init your app in this process.
-//            return;
-//        }
-//        LeakCanary.install(this);
     }
 }
--- app/src/main/java/com/lapsen/intelligentboiler/base/BaseActivity.java
+++ app/src/main/java/com/lapsen/intelligentboiler/base/BaseActivity.java
@@ -1,7 +1,15 @@
 package com.lapsen.intelligentboiler.base;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
@@ -25,6 +33,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestPermission();     //请求权限
         initViews(savedInstanceState);
     }
 
@@ -47,6 +56,40 @@ public abstract class BaseActivity extends AppCompatActivity {
                     finish();
                 }
             });
+        }
+    }
+
+    /**
+     * 动态获取权限
+     * */
+    private void requestPermission(){
+        if (Build.VERSION.SDK_INT >= 23){
+            int checkPermission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (checkPermission != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(BaseActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                //拒绝权限
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("存储权限");
+                builder.setMessage("你没有获取存储权限，可能会影响应用的使用");
+                builder.setPositiveButton("去获取", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermission();
+                    }
+                }).setNegativeButton("不获取，继续使用",null);
+                builder.create();
+                builder.show();
+            }
         }
     }
 }
